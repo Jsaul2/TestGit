7e26c9437f745e460ce1ff1ba04b9fc5eef7cb35
Job SAUL <job.saul2@gmail.com>
2017-12-05 15:09:39 +0100

    all files added

---
+++ TutoKarma_Jasmine/src/calc.js
@@ -0,0 +1,22 @@
+function calculatrice(signe, gauche, droite) {
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+           if (gauche>=0 && droite<430 && droite!=0) {
+            switch (signe) {
+                case "*":
+                    return gauche*droite;
+                case "+":
+                    return gauche+droite;
+                case "-":
+                    return gauche-droite;
+                case "/":
+                    return gauche/droite;
+                default:
+                    return null;
+            } 
+        } else {
+               return null;
+        }
+    } else{
+        return null;
+    }
+}
